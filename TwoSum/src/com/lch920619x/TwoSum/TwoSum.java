39489cbfb7305e35f7353d527e6a752056e4b971
lch <lch@172.23.24.155>
2017-10-30 17:06:18 +0800

    Added result output

--- TwoSum/src/com/lch920619x/TwoSum/TwoSum.java
+++ TwoSum/src/com/lch920619x/TwoSum/TwoSum.java
@@ -3,11 +3,13 @@ package com.lch920619x.TwoSum;
 public class TwoSum {
 
 	public static void main(String[] args) {
-      int[] arr = {1,5,7,3,4,7,0,3,3,7,8};
+      int[] arr = {1,5,7,2,4,7,0,3,3,7,8};
       int target = 7; 
       FindTwo finder = new FindTwo();
       int[] index = finder.find(arr, target);
-      System.out.println(index);
+      for (int i:index) {
+      System.out.println(i);
+      }
 
 	}
 
