4ded21d9ccd4bf4edcaf1dc8e3b26c18fd47c57b
lch <lch@172.23.24.155>
2017-10-30 17:03:59 +0800

    Add intial code

---
+++ TwoSum/src/com/lch920619x/TwoSum/FindTwo.java
@@ -0,0 +1,23 @@
+package com.lch920619x.TwoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindTwo {
+
+	public int[] find(int[] arr, int target) {
+		
+		Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+		for (int i=0; i< arr.length;i++) {
+
+			if(map.containsKey(target-arr[i])) {
+				int[] index = {map.get(target-arr[i]), i};
+				return index;
+			};
+			map.put(arr[i], i);
+			
+		}
+		
+		return null;
+	}
+}
--- TwoSum/src/com/lch920619x/TwoSum/TwoSum.java
+++ TwoSum/src/com/lch920619x/TwoSum/TwoSum.java
@@ -3,7 +3,11 @@ package com.lch920619x.TwoSum;
 public class TwoSum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+      int[] arr = {1,5,7,3,4,7,0,3,3,7,8};
+      int target = 7; 
+      FindTwo finder = new FindTwo();
+      int[] index = finder.find(arr, target);
+      System.out.println(index);
 
 	}
 
